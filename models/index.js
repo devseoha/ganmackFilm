85d850d15fdd245d5e6058817c3d9a6fa6aa46f1
mooo4030 <mooo4030@naver.com>
2021-05-26 21:11:06 +0900

    로그인구현

--- models/index.js
+++ models/index.js
@@ -5,31 +5,34 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+// const users = require('./users')
+// db.info_self_introduction_one_line_value1 = users;
+// users.init(sequelize);
+// users.associate(db);
 
 fs
   .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+        sequelize,
+        Sequelize.DataTypes
+    )
+    db[model.name] = model
+  })
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
