420d507e9f1e3567bf690bc865ba40be039befa4
mooo4030 <mooo4030@naver.com>
2021-05-28 14:02:33 +0900

    어바웃

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,20 @@ var db = require('../models/index.js');
 var config = require('../config/config.json');
 let jwt = require("jsonwebtoken");
 let cookie = require("cookie");
+var Sequelize = require('sequelize');
 
 // 간맥 페이지로 이동
-router.get('/', function(req, res) {
+router.get('/', async function(req, res) {
   let cookies = (req.headers.cookie !== undefined) ? cookie.parse(req.headers.cookie) : '';
   try{
     if(cookies.user){
-      res.render('main');
+      let list = await db.about_ganmack.findAll({
+        limit:10, 
+        order: [
+          ['no','desc']
+        ]
+      });
+      res.render('main',{list:list});
     }else{
       res.render('login');
     }
@@ -42,6 +49,18 @@ router.post('/login',async function(req, res) {
         {
           expiresIn: '5m'
         });
+
+        await db.user.update(
+        {
+          last_login_date : Sequelize.fn('now'),
+          token : token
+        },
+        {
+          where: {
+            id: id
+          }
+        });
+
         res.cookie("user", token).status(200).send(resResult(true, 200, "", {id:user.id}));
     }else{
       res.status(200).send(resResult(false, 400, "로그인안됨", "오지마세여"));
@@ -54,17 +73,24 @@ router.post('/login',async function(req, res) {
 
 // 어바웃 간맥 한줄쓰기
 router.post('/aboutGanmack',async function(req, res) {
+  let user_token = (req.headers.cookie.split('=')[0]=="user") ? req.headers.cookie.split('=')[1] : '';
   let {nickname, content} = req.body;
+  
   try{
-    // let user = await db.user.findOne({
-    //   attributes : [
-    //     'id',
-    //   ],
-    //   where: {
-    //     id: id,
-    //     password : password
-    //   }
-    // })
+    let user = await db.user.findOne({
+      attributes : [
+        'no',
+      ],
+      where: {
+        token : user_token
+      }
+    })
+
+    await db.about_ganmack.create({
+      nickname : nickname,
+      content : content,
+      user_no : user.no
+    })
     
     res.status(200).send(resResult(true, 200, "", ""));
   
